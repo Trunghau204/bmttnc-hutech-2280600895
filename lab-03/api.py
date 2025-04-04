63eb65df82738361361f923d3325cb8624557d04
Trần Trung Hậu <tranhau5065@gmail.com>
2025-04-04 09:53:38 +0700

    Add lab-03

--- lab-03/api.py
+++ lab-03/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from cipher.rsa import RSACipher
+from cipher.ecc import ECCCipher
 
 app = Flask(__name__)
 rsa_cipher = RSACipher()
+ecc_cipher = ECCCipher()
 
 @app.route('/api/rsa/generate_keys', methods=['GET'])
 def rsa_generate_keys():
@@ -63,30 +65,30 @@ def rsa_verify_signature():
     return jsonify({'is_valid': is_valid})
 
 
-# @app.route('/api/ecc/generate_keys', methods=['GET'])
-# def ecc_generate_keys():
-#     ecc_cipher.generate_keys()
-#     return jsonify({'message': 'Keys generated successfully'})
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
 
 
-# @app.route('/api/ecc/sign', methods=['POST'])
-# def ecc_sign_message():
-#     data = request.json
-#     message = data['message']
-#     private_key, _ = ecc_cipher.load_keys()
-#     signature = ecc_cipher.sign(message, private_key)
-#     signature_hex = signature.hex()
-#     return jsonify({'signature': signature_hex})
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    data = request.json
+    message = data['message']
+    private_key, _ = ecc_cipher.load_keys()
+    signature = ecc_cipher.sign(message, private_key)
+    signature_hex = signature.hex()
+    return jsonify({'signature': signature_hex})
 
-# @app.route('/api/ecc/verify', methods=['POST'])
-# def ecc_verify_signature():
-#     data = request.json
-#     message = data['message']
-#     signature_hex = data['signature']
-#     public_key, _ = ecc_cipher.load_keys()
-#     signature = bytes.fromhex(signature_hex)
-#     is_valid = ecc_cipher.verify(message, signature, public_key)
-#     return jsonify({'is_valid': is_valid})
+@app.route('/api/ecc/verify', methods=['POST'])
+def ecc_verify_signature():
+    data = request.json
+    message = data['message']
+    signature_hex = data['signature']
+    public_key, _ = ecc_cipher.load_keys()
+    signature = bytes.fromhex(signature_hex)
+    is_valid = ecc_cipher.verify(message, signature, public_key)
+    return jsonify({'is_valid': is_valid})
 
 
 
